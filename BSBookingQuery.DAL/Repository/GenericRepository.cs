2e9a03721d9f7baf0cf84293edc2f7370e8daf6c
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 21:38:11 +0600

    Add Generic Repository

---
+++ BSBookingQuery.DAL/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using BSBookingQuery.DAL.ApplicationDbContext;
+using BSBookingQuery.DAL.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BSBookingQuery.DAL.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly BSBookingQueryContext context;
+        public GenericRepository(BSBookingQueryContext context)
+        {
+            this.context = context;
+        }
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+        }
+        public void AddRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().AddRange(entities);
+        }
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            return context.Set<T>().Where(expression);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            return context.Set<T>().ToList();
+        }
+        public T GetById(int id)
+        {
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            return context.Set<T>().Find(id);
+        }
+        public void Remove(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().RemoveRange(entities);
+        }
+    }
+}
