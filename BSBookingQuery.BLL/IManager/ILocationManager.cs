d4aafd478c4eee3f4a486f00ec45e10333523592
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 22:51:14 +0600

    Add Location Manager and Some View Model

---
+++ BSBookingQuery.BLL/IManager/ILocationManager.cs
@@ -0,0 +1,16 @@
+﻿using BSBookingQuery.ViewModel.ViewModel;
+
+namespace BSBookingQuery.BLL.IManager
+{
+    public interface ILocationManager
+    {
+        Task<LocationViewModel> GetById(int id);
+        Task<IEnumerable<LocationViewModel>> GetAll();
+        Task Add(CreateViewModel entity);
+        Task AddRange(IEnumerable<CreateViewModel> entities);
+        void Update(UpdateViewModel entity);
+        void UpdateRange(IEnumerable<UpdateViewModel> entities);
+        void Delete(DeleteViewModel entity);
+        void DeleteRange(IEnumerable<DeleteViewModel> entities);
+    }
+}
---
+++ BSBookingQuery.BLL/Manager/LocationManager.cs
@@ -0,0 +1,57 @@
+﻿using BSBookingQuery.BLL.IManager;
+using BSBookingQuery.DAL.UnitOfWork;
+using BSBookingQuery.ViewModel.ViewModel;
+
+namespace BSBookingQuery.BLL.Manager
+{
+    public class LocationManager : ILocationManager
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public LocationManager(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public Task Add(CreateViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRange(IEnumerable<CreateViewModel> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(DeleteViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<DeleteViewModel> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LocationViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocationViewModel> GetById(int id)
+        {
+            LocationViewModel locationViewModel = new LocationViewModel();
+            var result=await unitOfWork.Location.GetById(id);
+            return locationViewModel;
+        }
+
+        public void Update(UpdateViewModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(IEnumerable<UpdateViewModel> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
