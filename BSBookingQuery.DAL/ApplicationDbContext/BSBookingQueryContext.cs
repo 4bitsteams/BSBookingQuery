2502984bab880eabb3b68e6ba784a956f102fa30
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 20:05:23 +0600

    Add Context BSBookingQueryContext

---
+++ BSBookingQuery.DAL/ApplicationDbContext/BSBookingQueryContext.cs
@@ -0,0 +1,28 @@
+﻿using BSBookingQuery.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BSBookingQuery.DAL.ApplicationDbContext
+{
+    public class BSBookingQueryContext
+        : DbContext
+    {
+        public BSBookingQueryContext(DbContextOptions options) : base(options) { }
+        public DbSet<Hotel> Hotel
+        {
+            get;
+            set;
+        }
+        public DbSet<Rating> Rating
+        {
+            get;
+            set;
+        }
+        public DbSet<Location> Location
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
--- BSBookingQuery.Entity/Models/Hotel.cs
+++ BSBookingQuery.Entity/Models/Hotel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BSBookingQuery.Entity.Models
+﻿namespace BSBookingQuery.Entity.Models
 {
     public class Hotel :BaseEntity
     {
