0b56fd83bddb3faf09e59d4c21ceada91da95b63
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 20:13:16 +0600

    Add DependencyInjection Class And Install Necessary Nuget Pakage

---
+++ BSBookingQuery.IOC/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using BSBookingQuery.DAL.ApplicationDbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BSBookingQuery.IOC.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection ImplementPersistence(this
+            IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BSBookingQueryContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(BSBookingQueryContext).Assembly.FullName)), ServiceLifetime.Transient);
+            return services;
+        }
+    }
+}
