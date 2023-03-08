c04cf257d63382fd7b0031e589b49879d3b96340
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 22:00:38 +0600

    Add Unit of Work

---
+++ BSBookingQuery.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using BSBookingQuery.DAL.IRepository;
+
+namespace BSBookingQuery.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IHotelRepository Hotel
+        {
+            get;
+        }
+        ICommentRepository Comment
+        {
+            get;
+        }
+        ILocationRepository Location
+        {
+            get;
+        }
+
+        IRatingRepository Rating
+        {
+            get;
+        }
+        int Save();
+    }
+}
---
+++ BSBookingQuery.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using BSBookingQuery.DAL.ApplicationDbContext;
+using BSBookingQuery.DAL.IRepository;
+using BSBookingQuery.DAL.Repository;
+
+namespace BSBookingQuery.DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private BSBookingQueryContext context;
+        public UnitOfWork(BSBookingQueryContext context)
+        {
+            this.context = context;
+            Hotel = new HotelRepository(this.context);
+            Comment = new CommentRepository(this.context);
+            Location = new LocationRepository(this.context);
+            Rating = new RatingRepository(this.context);
+        }
+        public IHotelRepository Hotel
+        {
+            get;
+        }
+        public ICommentRepository Comment
+        {
+            get;
+        }
+        public ILocationRepository Location
+        {
+            get;
+        }
+
+        public IRatingRepository Rating
+        {
+            get;
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+    }
+}
