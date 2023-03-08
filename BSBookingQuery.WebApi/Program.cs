caf59f84743b4637d1ed5e0f6b4eeee9a3472481
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 20:22:44 +0600

    Implement Infrasture DependencyInjection Container

--- BSBookingQuery.WebApi/Program.cs
+++ BSBookingQuery.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using BSBookingQuery.IOC.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Implement Infrasture DependencyInjection Container
+builder.Services.ImplementPersistence(builder.Configuration);
 
 var app = builder.Build();
 
