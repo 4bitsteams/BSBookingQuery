241e6dec18c82f4af1c13d0a2e6c8576d9fa2bf2
md.rubel <md.rubel@HOAP2406101764A>
2023-03-08 19:34:01 +0600

    Add Base Entity Class

---
+++ BSBookingQuery.Entity/Models/BaseEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BSBookingQuery.Entity.Models
+{
+    public class BaseEntity
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public int? DeletedBy { get; set; }
+
+        public int? DeletedDate { get; set; }
+
+        public Boolean IsActive { get; set; }
+
+        public Boolean IsDeleted { get; set; }
+
+    }
+}
