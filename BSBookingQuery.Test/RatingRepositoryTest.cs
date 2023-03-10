cf9745179cd67104351f432cd3901e945958fa69
md.rubel <md.rubel@HOAP2406101764A>
2023-03-11 01:00:57 +0600

    Refactor Code

---
+++ BSBookingQuery.Test/RatingRepositoryTest.cs
@@ -0,0 +1,31 @@
+namespace BSBookingQuery.Test
+{
+    [TestClass]
+    public class RatingRepositoryTest
+    {
+        [TestMethod]
+        public void get_all_async()
+        {
+        }
+
+        [TestMethod]
+        public void get_async()
+        {
+        }
+
+        [TestMethod]
+        public void add_rating()
+        {
+        }
+
+        [TestMethod]
+        public void update_rating()
+        {
+        }
+
+        [TestMethod]
+        public void delete_rating()
+        {
+        }
+    }
+}
